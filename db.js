b52e40d6304d6cc60bb34719c9080d268ed9f0d6
keppuraja3 <keppuraja3@gmail.com>
2024-07-17 11:47:33 +0530

    New: backend created

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const MONGODB_URL = process.env.MONGODB_URL
+
+async function connectDB(){
+    try {
+        await mongoose.connect(MONGODB_URL)
+        console.log("Database connected");
+    } catch (error) {
+        console.log("Database Error: ", error);
+        process.exit(1)
+    }
+
+}
+
+module.exports = { connectDB }
